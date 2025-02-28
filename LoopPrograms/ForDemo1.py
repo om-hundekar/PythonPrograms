64472566ba9de32717144b5e2aef723495168e85
om-hundekar <145151797+om-hundekar@users.noreply.github.com>
2025-02-28 12:08:52 +0530

    Add files via upload

---
+++ LoopPrograms/ForDemo1.py
@@ -0,0 +1,30 @@
+
+'''
+range(10):=>0,1,2,3,4,5,6,7,8,9
+range(1,10):=>1,2,3,4,5,6,7,8,9
+range(4,20,3):=>4,7,10,13,16
+range(10,1,-1):=>10,9,8,7,6,5,4,3,2
+'''
+
+#print all  no from 1-10 
+for no in range(10):
+    print(no)
+
+# even no between 1-100 
+for no in range(2,101,2):
+    print(no)
+
+for no in range(10,5,-1):
+    print(no)
+
+#print all characters separately
+
+for ch in "Hello World":
+    print(ch)
+
+
+#repet all char for 5 times
+
+for ch in "Hello World":
+    print(ch*5)
+    
